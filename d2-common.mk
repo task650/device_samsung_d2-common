24b06eecaee88deba48a31df00e3b111b425fb5f
task650 <task650@gmail.com>
2014-03-02 07:32:23 -0800

    Build DeviceParts

--- d2-common.mk
+++ d2-common.mk
@@ -157,6 +157,10 @@ endif
 PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
 
+#Build DeviceParts
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
 
